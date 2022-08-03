fa899523dd75b44dadc5f0de17bbd1e6ea47c55a
Kris <bloodlector@mail.ru>
2022-08-03 21:51:30 +0300

    lesson-1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Form } from "./components/funComponent/Form";
+
+export class App extends Component {
+  render() {
+    return <Form />;
+  }
+}
